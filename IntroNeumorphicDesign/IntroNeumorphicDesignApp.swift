126c7611175549d8d71ce84c652bd5a0b0bfbb79
domi802 <64065747+domi802@users.noreply.github.com>
2022-04-22 13:31:35 +0200

    Initial Commit

---
+++ IntroNeumorphicDesign/IntroNeumorphicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroNeumorphicDesignApp.swift
+//  IntroNeumorphicDesign
+//
+//  Created by Dominik Woźniak on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct IntroNeumorphicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
